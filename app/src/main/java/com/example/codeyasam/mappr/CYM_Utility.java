aa1852710a179edf6ddcb11582a1448628fc9c53
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-06 20:14:47 +0800

    renames and adds functionalities to CYM_Utility

---
+++ app/src/main/java/com/example/codeyasam/mappr/CYM_Utility.java
@@ -0,0 +1,86 @@
+package com.example.codeyasam.mappr;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by codeyasam on 6/1/16.
+ */
+public class CYM_Utility {
+
+    public static final String MAPPR_ROOT_URL = "http://192.168.42.127/thesis/";
+    public static final String MAPPR_PUBLIC_URL = MAPPR_ROOT_URL + "Public/";
+    public static final String MAPPR_OPT = "MAPPR_OPT";
+    public static final String OPT_BY_QRCODE = "111";
+    public static final String OPT_BY_CATEGORY = "777";
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        // canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+
+        canvas.drawCircle(bitmap.getWidth() / 2, bitmap.getHeight() / 2,
+                bitmap.getWidth() / 2, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        //Bitmap _bmp = Bitmap.createScaledBitmap(output, 60, 60, false);
+        //return _bmp;
+        return output;
+    }
+
+    public static float dipToPixels(Context context, float dipValue) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dipValue, metrics);
+    }
+
+    public static Bitmap loadImageFromServer(String url) {
+        try {
+            URL urlConnection = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) urlConnection
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void displayText(Activity activity, int id, String text) {
+        TextView tv = (TextView) activity.findViewById(id);
+        tv.setText(text);
+    }
+
+    public static void setImageOnView(Activity activity, int id, Bitmap bmp) {
+        ImageView iv = (ImageView) activity.findViewById(id);
+        iv.setImageBitmap(bmp);
+    }
+}
