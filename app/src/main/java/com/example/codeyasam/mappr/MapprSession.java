47f2c7dd404f0ae1802af3964d0718b01aca1f40
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-10 02:53:38 +0800

    adds session class model

---
+++ app/src/main/java/com/example/codeyasam/mappr/MapprSession.java
@@ -0,0 +1,13 @@
+package com.example.codeyasam.mappr;
+
+/**
+ * Created by codeyasam on 6/8/16.
+ */
+public class MapprSession {
+
+    public static boolean isLoggedIn = false;
+
+    public static final String REGISTERORIGIN = "registerFrom";
+    public static final String RATEREGISTER = "rateregister101";
+
+}
