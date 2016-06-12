d996de38810c7eb75f54c8b30cb66fb596621595
Emmanuel Yasa <codeyasam@gmail.com>
2016-06-12 20:15:10 +0800

    [MapprSession] adds logUser functionality where in it stored the user id in the shared preferences

--- app/src/main/java/com/example/codeyasam/mappr/MapprSession.java
+++ app/src/main/java/com/example/codeyasam/mappr/MapprSession.java
@@ -1,5 +1,9 @@
 package com.example.codeyasam.mappr;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
 /**
  * Created by codeyasam on 6/8/16.
  */
@@ -9,5 +13,15 @@ public class MapprSession {
 
     public static final String REGISTERORIGIN = "registerFrom";
     public static final String RATEREGISTER = "rateregister101";
+    public static final String LOGGED_USER_ID = "logged_user_id";
+
+    public static void logUser(Activity activity, String userId) {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(activity);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(LOGGED_USER_ID, userId);
+        editor.commit();
+        isLoggedIn = true;
+    }
+
 
 }
